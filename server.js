d00292a086b8910322697619bc6e50d59cf92b8f
HemantWD <hemantajaysharma@gmail.com>
2024-04-12 20:36:22 +0530

    deployement

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 // config env
 dotenv.config();
@@ -20,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -29,12 +31,11 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Ecommerce App</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // PORT
-
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(
